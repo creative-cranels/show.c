7c556ee850920495afebea7c786be5eb85b3338c
Azamat Mukhamejanov <azamat@sprintsquads.com>
2023-09-12 14:08:23 +0600

    docs: add explanation comments and error message when directory is not opened

--- show.c
+++ show.c
@@ -1,24 +1,31 @@
-#include <stdio.h>
+#include <stdio.h> // io library
 #include <dirent.h>
 
+/*
+  main() - Prints the directory contents in stdout
+  argc - number of arguments
+  argv - list of arguments
+*/ 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("No path provided");
+    printf("ERR: No path provided\n");
     return -1;
   }
 
   char *path=argv[1];
-  printf("Listing files and directories in: %s\n", path);
 
-
-  DIR *d;
-  struct dirent *dir;
+  DIR *d; // DIR - directory stream type
   d = opendir(path);
   if (d) {
+    printf("Listing files and directories in: %s\n", path);
+    struct dirent *dir;
     while ((dir = readdir(d)) != NULL) {
       printf("%s\n", dir->d_name);
     }
     closedir(d);
+  } else {
+    printf("ERR: Path '%s' doesn't exist\n", path);
+    return -1;
   }
 
   return 0;
